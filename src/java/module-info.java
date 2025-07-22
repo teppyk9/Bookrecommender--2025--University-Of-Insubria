a61d35355a572378dca5efbae05cf2e8cddab115
teppyk9 <gmteppa99@gmail.com>
2025-07-22 11:51:08 +0200

    fix

--- src/java/module-info.java
+++ src/java/module-info.java
@@ -1,33 +1,33 @@
 module bookrecommender {
     requires java.rmi;
     requires java.sql;
-    requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
     requires org.postgresql.jdbc;
     requires java.desktop;
     requires com.zaxxer.hikari;
+    requires javafx.graphics;
+    requires javafx.controls;
 
     opens bookrecommender.client to javafx.fxml;
+    opens bookrecommender.client.ui to javafx.fxml;
+    opens bookrecommender.client.util to javafx.fxml;
+    opens bookrecommender.client.enums to javafx.fxml;
+    opens bookrecommender.client.listener to javafx.fxml;
     opens bookrecommender.server to javafx.fxml;
+    opens bookrecommender.server.ui to javafx.fxml;
+    opens bookrecommender.server.dao to javafx.fxml;
+    opens bookrecommender.server.service to javafx.fxml;
+    opens bookrecommender.server.util to javafx.fxml;
     exports bookrecommender.client;
+    exports bookrecommender.client.ui;
+    exports bookrecommender.client.util;
+    exports bookrecommender.client.enums;
+    exports bookrecommender.client.listener;
     exports bookrecommender.server;
     exports bookrecommender.common.interfaces;
     exports bookrecommender.common.model;
     exports bookrecommender.server.ui;
-    opens bookrecommender.server.ui to javafx.fxml;
     exports bookrecommender.server.dao;
-    opens bookrecommender.server.dao to javafx.fxml;
     exports bookrecommender.server.service;
-    opens bookrecommender.server.service to javafx.fxml;
     exports bookrecommender.server.util;
-    opens bookrecommender.server.util to javafx.fxml;
-    exports bookrecommender.client.ui;
-    opens bookrecommender.client.ui to javafx.fxml;
-    exports bookrecommender.client.util;
-    opens bookrecommender.client.util to javafx.fxml;
-    exports bookrecommender.client.enums;
-    opens bookrecommender.client.enums to javafx.fxml;
-    exports bookrecommender.client.listener;
-    opens bookrecommender.client.listener to javafx.fxml;
 }
